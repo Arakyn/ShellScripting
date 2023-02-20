511fc1ed1732dea2ed50cbbfc274d306e5137edb
Arakyn <wizzyt2222@gmail.com>
2023-02-20 18:15:07 +0530

    ShellScripting

---
+++ 03-CommentsAndVariables/yes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Adding a comment right here
+
+echo "Hello Guys" #this a comment too
+
+# now we talk about variables
+
+# there are 2 types of variables 
+# system variables and user variables
+# system variables are predefined variables
+
+## USER DEFINED Variables
+# User defined variables are the ones we make
+
+echo $BASH
+echo $BASH_VERSION
+echo $PWD 
+
+myname=Mark
+
+echo $PWD
+
+echo Hi, my name is $myname
